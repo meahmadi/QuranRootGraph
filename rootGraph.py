198c7c28b127e4a316de9a453b23be31c3d5e8fd
Meisam Ahmadi <me.ahmadi@gmail.com>
2016-07-16 15:13:00 +0430

    s

--- rootGraph.py
+++ rootGraph.py
@@ -1,4 +1,10 @@
 
+def parseQuranMetadata(resourceFile):
+    import json
+    with open(resourceFile) as f:
+        metadata = json.loads(f.read())
+    return metadata
+
 def parseQuranTokens(corpusFile):
     tokens = {}
     with open(corpusFile) as file:
@@ -45,6 +51,7 @@ def getRootGraph(roots):
     return graph
 
 def ayeSimilarity(graph,a,b):
+    import math
     d = 0
     for wordA in a.keys():
         for tokenA in a[wordA].keys():
@@ -55,10 +62,24 @@ def ayeSimilarity(graph,a,b):
                             d = d + graph[ a[wordA][tokenA]["ROOT"] ][ b[wordB][tokenB]["ROOT"] ]
                         except:
                             pass
+    d = d / math.sqrt(len(a)*len(b))
     return d
 
-def sureSelfSimilarity(graph,s):
+def siaqSimilarity(graph,s1,s11,s12,s2,s21,s22):
+    d = 0
+    for ayeA in s1:
+        if int(ayeA) < s11 or int(ayeA) >  s12:
+            continue
+        for ayeB in s2:
+            if int(ayeB) <s21 or int(ayeB) > s22:
+                continue
+            d = d + ayeSimilarity(graph,s1[ayeA],s2[ayeB])
+    d = d / ((s12-s11+1) * (s22-s21 + 1))
+    return d
+
+def sureSelfSimilarity(graph, metadata, tokens, sure):
     r = {}
+    s = tokens[sure]
     ayat = list(s.keys())
     ayat.sort()
     for ayeA in ayat:
@@ -67,7 +88,39 @@ def sureSelfSimilarity(graph,s):
             if ayeA is not ayeB:
                 ts = ayeSimilarity(graph,s[ayeA],s[ayeB])
                 r[ayeA] = r[ayeA] + ts
-    return r
+    sortedAyat = sorted(r, key=r.get)[::-1]
+    print("ayat:",sortedAyat)
+
+    siaqs = sorted([s[1] for s in metadata["Ruku"] if len(s)>0 and s[0] is int(sure)])
+    siaqs.append(len(s))
+    print("siaqs:",siaqs)
+
+    sq = {}
+    for i1 in range(len(siaqs)-1):
+        s11 = siaqs[i1]
+        s12 = siaqs[i1+1]-1
+        sq[(s11,s12)] = 0
+        for i2 in range(len(siaqs)-1):
+            if i2 is i1:
+                continue
+            s21 = siaqs[i2]
+            s22 = siaqs[i2+1]-1
+
+            ts = siaqSimilarity(graph,s,s11,s12,s,s21,s22)
+            sq[(s11,s12)] = sq[(s11,s12)] + ts
+        sq[(s11,s12)] = sq[(s11,s12)]/(s12-s11+1)
+    print("siaq:",sq)
+    maxsq = max(sq.values())
+    siaqAyat = []
+    for k in sq:
+        if sq[k] == maxsq:
+            siaqAyat = [ x for x in sortedAyat if ((int(x)>=k[0]) and (int(x)<=k[1])) ]
+        sq[k] = sq[k]/maxsq
+
+    print("siaq:",sq)
+    print("best siaq:",siaqAyat)
+
+    return siaqAyat[0]
 
 def saveTo(d,filename):
     import json
@@ -93,6 +146,7 @@ def saveGraphToGDF(graph,gfdfile):
 
 if __name__ == '__main__':
     import os.path
+
     corpusfile = "corpus.json"
     if os.path.exists(corpusfile):
         tokens = loadFrom(corpusfile)
@@ -100,6 +154,8 @@ if __name__ == '__main__':
         tokens = parseQuranTokens("quranic-corpus-morphology-0.4.txt")
         saveTo(tokens,corpusfile)
     print("tokens:", len(tokens))
+
+    metadata = parseQuranMetadata("quran-data.js")
 
     rootfile = "roots.json"
     if os.path.exists(rootfile):
@@ -109,6 +165,7 @@ if __name__ == '__main__':
         saveTo(roots,rootfile)
     print("roots:",len(roots))
 
+
     graphfile = "rootgraph.json"
     if os.path.exists(graphfile):
         rootGraph = loadFrom(graphfile)
@@ -117,6 +174,5 @@ if __name__ == '__main__':
         saveTo(rootGraph,graphfile)
         saveGraphToGDF(rootGraph,"rootGraph.gdf")
 
-    yasinSims = sureSelfSimilarity(rootGraph, tokens["36"])
-    sortedAyat = sorted(yasinSims, key=yasinSims.get)[::-1]
-    print("yasin ayat:",sortedAyat)
+    yasinSims = sureSelfSimilarity(rootGraph, metadata, tokens, "36")
+    yasinSims = sureSelfSimilarity(rootGraph, metadata, tokens, "2")
